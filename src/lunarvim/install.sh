c185ee90c7ac6a669f878e45891ee8d61bba5e72
Oleksii Shevchenko <rembo.51423@gmail.com>
2024-10-22 17:23:28 +0300

    feat(lunarvim): remove lunarvim from src & test dirs

--- src/lunarvim/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Activating feature 'lunarvim'"
-
-export INSTALL_PREFIX=/usr/local
-curl -sL "https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh" | bash -s -- --no-install-dependencies -y 1>/dev/null;
